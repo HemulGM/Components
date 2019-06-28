cdaec83bf5f2555830abadacede5495ef2314e00
HemulGM <alinvip22@gmail.com>
2019-06-28 15:59:38 +0700

    Popup V.UP

--- HGM.Popup.dfm
+++ HGM.Popup.dfm
@@ -4,7 +4,7 @@ object FormPopup: TFormPopup
   BorderStyle = bsNone
   ClientHeight = 532
   ClientWidth = 506
-  Color = 4210752
+  Color = clBtnFace
   DoubleBuffered = True
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -21,9 +21,63 @@ object FormPopup: TFormPopup
   Visible = True
   OnActivate = FormActivate
   OnClick = FormClick
-  OnCreate = FormCreate
+  OnClose = FormClose
   OnHide = FormHide
   OnShow = FormShow
   PixelsPerInch = 96
   TextHeight = 13
+  object imgUpArrow: TImage
+    Left = 1
+    Top = 1
+    Width = 504
+    Height = 12
+    Align = alTop
+    Center = True
+    Picture.Data = {
+      0954506E67496D61676589504E470D0A1A0A0000000D49484452000000170000
+      000C0806000000893B26F8000000097048597300000B1300000B1301009A9C18
+      0000005E4944415478DAB58DC90D00200804A510FA2F8C42343E341E888B072F
+      583633141CC3CC514408EDC3C50C2E3B2A804A2DD823D81634302A309F161811
+      2C1F08782750430FD8124CC1097825E88E1BB026A8CB0BF028A0D7E056403FC0
+      6512EAD82D637D5D06CA0000000049454E44AE426082}
+    Visible = False
+  end
+  object imgDownArrow: TImage
+    Left = 1
+    Top = 519
+    Width = 504
+    Height = 12
+    Align = alBottom
+    Center = True
+    Picture.Data = {
+      0954506E67496D61676589504E470D0A1A0A0000000D49484452000000170000
+      000C0806000000893B26F8000000097048597300000B1300000B1301009A9C18
+      0000005D4944415478DAB5CEBB0D00200845511984FD0763100D05C6A8C8F347
+      4142732EC4CC397D1AD2F52320224476BC0C285C3F7F193078C06F032D3CC54F
+      033DECE2BB8119BCC4D1800787781458C110EE052218C6FB00026FE11640619D
+      0244EC2D63CA9DA8B70000000049454E44AE426082}
+    Visible = False
+    ExplicitLeft = 2
+    ExplicitTop = 9
+  end
+  object Shape1: TShape
+    Left = 1
+    Top = 13
+    Width = 504
+    Height = 506
+    Align = alClient
+    Brush.Color = 1907997
+    Pen.Color = 1907997
+    ExplicitLeft = 176
+    ExplicitTop = 176
+    ExplicitWidth = 65
+    ExplicitHeight = 65
+  end
+  object tmrAutoHide: TTimer
+    Enabled = False
+    Interval = 5000
+    OnTimer = tmrAutoHideTimer
+    Left = 184
+    Top = 184
+  end
 end
--- HGM.Popup.pas
+++ HGM.Popup.pas
@@ -3,24 +3,42 @@ unit HGM.Popup;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
+  System.Classes, Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs,
+  Vcl.ExtCtrls, Vcl.Imaging.pngimage;
 
 type
+  TPopupStyles = (psShowArrow, psAnimate, psShadow, psFrame);
+  TPopupStyle = set of TPopupStyles;
+
   TFormPopup = class(TForm)
+    imgUpArrow: TImage;
+    imgDownArrow: TImage;
+    Shape1: TShape;
+    tmrAutoHide: TTimer;
     procedure FormHide(Sender: TObject);
-    procedure FormCreate(Sender: TObject);
     procedure FormShow(Sender: TObject);
     procedure FormActivate(Sender: TObject);
     procedure FormClick(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure tmrAutoHideTimer(Sender: TObject);
   protected
-   procedure WmActivate(var Msg: TWMActivate); message WM_ACTIVATE;
+    procedure WmActivate(var Msg: TWMActivate); message WM_ACTIVATE;
   private
-   FOwner:TForm;
-   FControl:TWinControl;
-   procedure DoRelease;
+    FOwner: TForm;
+    FControl: TWinControl;
+    FAnimate: Boolean;
+    FClosing: Boolean;
+    FFreeMethod: TProc;
+    procedure DoRelease;
   public
-   constructor Create(AOwner:TForm; AControl:TWinControl; X, Y:Integer); overload;
+    constructor CreatePopup(AOwner: TForm; AControl: TWinControl; FreeMethod: TProc;
+                            X, Y: Integer;
+                            Style: TPopupStyle); overload;
+    constructor CreateDown(AOwner: TForm; AControl: TWinControl; FreeMethod: TProc;
+                           X, Y: Integer;
+                           Style: TPopupStyle;
+                           HideAfter: Integer = 0); overload;
   end;
 
 implementation
@@ -29,65 +47,136 @@ implementation
 
 { TFormPopup }
 
-procedure TFormPopup.DoRelease;
+constructor TFormPopup.CreateDown(AOwner: TForm; AControl: TWinControl; FreeMethod: TProc; X, Y: Integer; Style: TPopupStyle; HideAfter: Integer);
 begin
- FControl.Hide;
- Winapi.Windows.SetParent(FControl.Handle, FOwner.Handle);
- Release;
+  CreatePopup(AOwner, AControl, FreeMethod, X, Y, Style);
+  if HideAfter > 0 then
+  begin
+    tmrAutoHide.Interval := HideAfter;
+    tmrAutoHide.Enabled := True;
+  end;
+  imgUpArrow.Hide;
+  imgDownArrow.Show;
+  Top := Top - Height;
+  FControl.Top := 1;
 end;
 
-constructor TFormPopup.Create(AOwner: TForm; AControl:TWinControl; X, Y:Integer);
+procedure TFormPopup.DoRelease;
 begin
- inherited Create(AOwner);
- FOwner:=AOwner;
- FControl:=AControl;
- Winapi.Windows.SetParent(AControl.Handle, Handle);
- Left:=X;
- Top:=Y;
- ClientWidth:=AControl.Width+2;
- ClientHeight:=AControl.Height+2;
- AControl.Left:=1;
- AControl.Top:=1;
- AControl.Show;
- AControl.BringToFront;
- AControl.SetFocus;
+  FControl.Hide;
+  Winapi.Windows.SetParent(FControl.Handle, FOwner.Handle);
+  Release;
+end;
+
+constructor TFormPopup.CreatePopup(AOwner: TForm; AControl: TWinControl; FreeMethod: TProc; X, Y: Integer; Style: TPopupStyle);
+const
+  CS_DROPSHADOW = $00020000;
+begin
+  inherited Create(AOwner);
+  FClosing := False;
+  FOwner := AOwner;
+  FControl := AControl;
+  FAnimate := psAnimate in Style;
+  FFreeMethod := FreeMethod;
+  Winapi.Windows.SetParent(FControl.Handle, Handle);
+  Left := X;
+  Top := Y;
+  if psShowArrow in Style then
+  begin
+    if psShadow in Style then SetClassLong(Handle, GCL_STYLE, GetWindowLong(Handle, GCL_STYLE) and CS_DROPSHADOW);
+    ClientWidth := FControl.Width;
+    ClientHeight := FControl.Height + imgUpArrow.Height;
+    FControl.Left := 0;
+    FControl.Top := imgUpArrow.Height;
+    Color := clFuchsia;
+    TransparentColor := True;
+    imgUpArrow.Show;
+  end
+  else
+  begin
+    if psShadow in Style then
+    begin
+     SetClassLong(Handle, GCL_STYLE, GetWindowLong(Handle, GCL_STYLE) or CS_DROPSHADOW);
+     Color := $00404040;
+    end
+    else
+    begin
+     Shape1.Hide;
+     Color := clBtnFace;
+    end;
+    ClientWidth := FControl.Width;
+    ClientHeight := FControl.Height;
+    FControl.Left := 0;
+    FControl.Top := 0;
+    TransparentColor := False;
+  end;
+  if psFrame in Style then
+  begin
+    FControl.Left := FControl.Left + 1;
+    FControl.Top := FControl.Top + 1;
+    ClientWidth := ClientWidth + 2;
+    ClientHeight := ClientHeight + 2;
+  end;
+  FControl.Show;
+  FControl.BringToFront;
+  FControl.SetFocus;
 end;
 
 procedure TFormPopup.FormActivate(Sender: TObject);
 begin
- SendMessage(FOwner.Handle, WM_NCACTIVATE, Integer(True), 0);
+  SendMessage(FOwner.Handle, WM_NCACTIVATE, Integer(True), 0);
 end;
 
 procedure TFormPopup.FormClick(Sender: TObject);
 begin
- Close;
+  Close;
 end;
 
-procedure TFormPopup.FormCreate(Sender: TObject);
-const CS_DROPSHADOW = $00020000;
+procedure TFormPopup.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
- SetClassLong(Handle, GCL_STYLE, GetWindowLong(Handle, GCL_STYLE) or CS_DROPSHADOW);
+  if FClosing then
+  begin
+    Action := caNone;
+    Exit;
+  end
+  else
+   if Assigned(FFreeMethod) then FFreeMethod;
+  FClosing := True;
 end;
 
 procedure TFormPopup.FormHide(Sender: TObject);
 begin
- AnimateWindow(Handle, 100, AW_BLEND or AW_HIDE);
- DoRelease;
+  if FAnimate then
+    AnimateWindow(Handle, 100, AW_BLEND or AW_HIDE);
+  DoRelease;
 end;
 
 procedure TFormPopup.FormShow(Sender: TObject);
 begin
- if Left + Width > Screen.DesktopRect.Right then Left:=Screen.DesktopRect.Right - Width;
- if Top + Height > Screen.DesktopRect.Bottom then Top:=Screen.DesktopRect.Bottom - Height;
+  if Left + Width > Screen.DesktopRect.Right then
+    Left := Screen.DesktopRect.Right - Width;
+  if Top + Height > Screen.DesktopRect.Bottom then
+    Top := Screen.DesktopRect.Bottom - Height;
 
- AnimateWindow(Handle, 100, AW_BLEND);
+  if Left < 0 then
+    Left := 0;
+  if Top < 0 then
+    Top := 0;
+  if FAnimate then
+    AnimateWindow(Handle, 100, AW_BLEND);
+end;
+
+procedure TFormPopup.tmrAutoHideTimer(Sender: TObject);
+begin
+  if not Self.BoundsRect.Contains(Mouse.CursorPos) then
+    Close;
 end;
 
 procedure TFormPopup.WmActivate(var Msg: TWMActivate);
 begin
- SendMessage(FOwner.Handle, WM_NCACTIVATE, Ord(Msg.Active <> WA_INACTIVE), 0);
- inherited;
- if Msg.Active = WA_INACTIVE then
+  SendMessage(FOwner.Handle, WM_NCACTIVATE, Ord(Msg.Active <> WA_INACTIVE), 0);
+  inherited;
+  if Msg.Active = WA_INACTIVE then
     DoRelease;
 end;
 
