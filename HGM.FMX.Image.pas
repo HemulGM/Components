fbce6934736979d1b7a754ffa95566201f4e3f74
HemulGM <alinvip22@gmail.com>
2023-05-04 00:06:11 +0500

    Update FMX.Image

--- HGM.FMX.Image.pas
+++ HGM.FMX.Image.pas
@@ -4,27 +4,10 @@ interface
 
 uses
   System.Classes, System.Types, System.SysUtils, FMX.Forms, FMX.Graphics,
-  FMX.Objects, System.Threading, System.Generics.Collections;
+  FMX.Objects, System.Threading, System.Generics.Collections,
+  System.Net.HttpClient;
 
 type
-  TBitmapCacheItem = class
-  private
-    FImage: TBitmap;
-    FUrl: string;
-    FLoaded: Boolean;
-    procedure SetImage(const Value: TBitmap);
-    procedure SetLoaded(const Value: Boolean);
-    procedure SetUrl(const Value: string);
-  public
-    constructor Create;
-    destructor Destroy; override;
-    property Image: TBitmap read FImage write SetImage;
-    property Url: string read FUrl write SetUrl;
-    property Loaded: Boolean read FLoaded write SetLoaded;
-  end;
-
-  TBitmapCache = TArray<TBitmapCacheItem>;
-
   TCallbackObject = record
     Owner: TComponent;
     Bitmap: TBitmap;
@@ -39,41 +22,29 @@ type
   private
     class var
       Pool: TThreadPool;
-      DroppingCache: Boolean;
       FCallbackList: TThreadList<TCallbackObject>;
       FObjectOwner: TComponent;
+      FClient: THTTPClient;
     class procedure AddCallback(Callback: TCallbackObject);
-    class procedure RemoveCallback(const AOwner: TComponent);
     class procedure Ready(const Url: string; Stream: TStream);
-    class function AppendCache: TBitmapCacheItem;
-    class procedure DropCacheLimit;
-    class function ExistsCache(const Url: string; var Target: TBitmap): Boolean;
+    class function Get(const URL: string): TMemoryStream; static;
+    class function GetClient: THTTPClient;
   public
-    class var
-      PictureCache: TBitmapCache;
-      LoadCounter: Integer;
-      LoadCounterLimit: Integer;
-      CacheSize: Integer;
-      GlobalUseCache: Boolean;
-    function LoadFromUrl(const Url: string; UseCache: Boolean = True): Boolean;
-    function LoadFromUrlAsync(const Url: string; UseCache: Boolean = True; AfterLoaded: TProc<TBitmap> = nil): Boolean; overload;
+    class procedure RemoveCallback(const AOwner: TComponent);
+    procedure LoadFromUrl(const Url: string; UseCache: Boolean = True);
     procedure LoadFromUrlAsync(AOwner: TComponent; const Url: string; Cache: Boolean = True); overload;
     procedure LoadFromResource(ResName: string); overload;
     procedure LoadFromResource(Instanse: NativeUInt; ResName: string); overload;
     procedure SaveToStream(Stream: TStream; const Ext: string); overload;
     procedure SaveToFile(const AFileName: string; const Ext: string); overload;
-    class procedure DropCache;
-    class procedure SetLoaded(Url: string);
-    class procedure DeleteCache(Url: string);
     class function CreateFromUrl(const Url: string; UseCache: Boolean = True): TBitmap;
-    class function CreateLazy(const Url: string; FirstAsDefault: Boolean = False; UseCache: Boolean = True): TBitmap;
     class function CreateFromResource(ResName: string; Url: string = ''): TBitmap;
   end;
 
 implementation
 
 uses
-  HGM.Common.Download, FMX.Surfaces, FMX.Types, FMX.Consts;
+  FMX.Surfaces, FMX.Types, FMX.Consts;
 
 { TBitmapHelper }
 
@@ -83,127 +54,16 @@ begin
   FCallbackList.Add(Callback);
 end;
 
-class function TBitmapHelper.AppendCache: TBitmapCacheItem;
-begin
-  DropCacheLimit;
-  Result := TBitmapCacheItem.Create;
-  SetLength(PictureCache, Length(PictureCache) + 1);
-  PictureCache[High(PictureCache)] := Result;
-end;
-
-class procedure TBitmapHelper.DropCache;
-var
-  i: Integer;
-begin
-  for i := Low(PictureCache) to High(PictureCache) do
-    PictureCache[i].Free;
-  SetLength(PictureCache, 0);
-end;
-
-class procedure TBitmapHelper.DropCacheLimit;
-begin
-  if GlobalUseCache and not DroppingCache then
-  begin
-    DroppingCache := True;
-    try
-      while Length(PictureCache) > CacheSize do
-      begin
-        PictureCache[0].Free;
-        Delete(PictureCache, 0, 1);
-      end;
-    except
-    end;
-    DroppingCache := False;
-  end;
-end;
-
 class function TBitmapHelper.CreateFromResource(ResName, Url: string): TBitmap;
-var
-  Item: TBitmapCacheItem;
 begin
-  if GlobalUseCache then
-  begin
-    Item := AppendCache;
-    Item.Image := TBitmap.Create;
-    Item.Loaded := True;
-    Item.Url := Url;
-    Item.Image.LoadFromResource(ResName);
-    Result := Item.Image;
-  end
-  else
-  begin
-    Result := TBitmap.Create;
-    Result.LoadFromResource(ResName);
-  end;
+  Result := TBitmap.Create;
+  Result.LoadFromResource(ResName);
 end;
 
 class function TBitmapHelper.CreateFromUrl(const Url: string; UseCache: Boolean): TBitmap;
-var
-  Item: TBitmapCacheItem;
 begin
-  if GlobalUseCache and UseCache and ExistsCache(Url, Result) then
-    Exit;
-  if GlobalUseCache then
-  begin
-    Item := AppendCache;
-    Item.Image := TBitmap.Create;
-    Item.Loaded := True;
-    Item.Url := Url;
-    Item.Image.LoadFromUrl(Url, False);
-    Result := Item.Image;
-  end
-  else
-  begin
-    Result := TBitmap.Create;
-    Result.LoadFromUrl(Url, False);
-  end;
-end;
-
-class function TBitmapHelper.CreateLazy(const Url: string; FirstAsDefault: Boolean; UseCache: Boolean): TBitmap;
-var
-  Item: TBitmapCacheItem;
-begin
-  if GlobalUseCache and UseCache and ExistsCache(Url, Result) then
-    Exit;
-  Item := AppendCache;
-  Item.Image := TBitmap.Create;
-  Item.Url := Url;
-  if FirstAsDefault then
-  begin
-    if Length(PictureCache) > 1 then
-      Item.Image.Assign(PictureCache[0].Image);
-  end;
-  Result := Item.Image;
-end;
-
-class procedure TBitmapHelper.DeleteCache(Url: string);
-var
-  i: Integer;
-begin
-  for i := Low(PictureCache) to High(PictureCache) do
-  begin
-    if PictureCache[i].Url = Url then
-    begin
-      PictureCache[i].Free;
-      Delete(PictureCache, i, 1);
-      Exit;
-    end;
-  end;
-end;
-
-class function TBitmapHelper.ExistsCache(const Url: string; var Target: TBitmap): Boolean;
-var
-  i: Integer;
-begin
-  Result := False;
-  for i := Low(PictureCache) to High(PictureCache) do
-  begin
-    if PictureCache[i].Url = Url then
-    begin
-      Target := PictureCache[i].Image;
-      Exit(True);
-    end;
-  end;
+  Result := TBitmap.Create;
+  Result.LoadFromUrl(Url, False);
 end;
 
 procedure TBitmapHelper.LoadFromResource(ResName: string);
@@ -223,48 +83,40 @@ begin
   end;
 end;
 
-function TBitmapHelper.LoadFromUrl(const Url: string; UseCache: Boolean): Boolean;
+procedure TBitmapHelper.LoadFromUrl(const Url: string; UseCache: Boolean);
 var
   Mem: TMemoryStream;
-  Item: TBitmapCacheItem;
-  Cache: TBitmap;
-  FLoaded: Boolean;
 begin
-  Result := False;
-  if GlobalUseCache and UseCache and ExistsCache(Url, Cache) then
-  begin
-    Self.Assign(Cache);
-    Exit(True);
-  end;
-  Mem := TDownload.Get(Url);
+  Mem := Get(Url);
   try
-    try
-      if Mem.Size > 0 then
-      begin
-        TThread.Synchronize(nil,
-          procedure
-          begin
-            try
-              Self.LoadFromStream(Mem);
-              FLoaded := True;
-            except
-              FLoaded := False;
-            end;
-          end);
-        Result := FLoaded;
-        if GlobalUseCache and UseCache then
-        begin
-          Item := AppendCache;
-          Item.Image := Self;
-          Item.Url := Url;
-          Item.Loaded := True;
-        end;
-      end;
-    finally
-      Mem.Free;
-    end;
-  except
+    Self.LoadFromStream(Mem);
+  finally
+    Mem.Free;
   end;
+end;
+
+class function TBitmapHelper.Get(const URL: string): TMemoryStream;
+begin
+  if URL.IsEmpty then
+    raise Exception.Create('Empty URL');
+  Result := TMemoryStream.Create;
+  try
+    if (GetClient.Get(URL, Result).StatusCode = 200) and (Result.Size > 0) then
+      Result.Position := 0;
+  except
+    Result.Free;
+    Result := nil;
+  end;
+end;
+
+class function TBitmapHelper.GetClient: THTTPClient;
+begin
+  if not Assigned(FClient) then
+  begin
+    FClient := THTTPClient.Create;
+    FClient.HandleRedirects := True;
+  end;
+  Result := FClient;
 end;
 
 procedure TBitmapHelper.LoadFromUrlAsync(AOwner: TComponent; const Url: string; Cache: Boolean);
@@ -278,7 +130,7 @@ begin
     procedure
     begin
       try
-        var Mem := TDownload.Get(Url);
+        var Mem := Get(Url);
         //if Cache then
         //  AddCache(Url, Mem);
         TThread.ForceQueue(nil,
@@ -351,38 +203,6 @@ begin
   end;
 end;
 
-function TBitmapHelper.LoadFromUrlAsync(const Url: string; UseCache: Boolean; AfterLoaded: TProc<TBitmap>): Boolean;
-var
-  Cache: TBitmap;
-begin
-  if GlobalUseCache and UseCache and ExistsCache(Url, Cache) then
-  begin
-    Self.Assign(Cache);
-    Exit(True);
-  end;
-  Result := False;
-  TTask.Run(
-    procedure
-    begin
-      while LoadCounter > LoadCounterLimit do
-        TThread.Sleep(500);
-      Inc(LoadCounter);
-      try
-        Self.LoadFromUrl(Url, UseCache);
-      except
-      end;
-      Dec(LoadCounter);
-      if Assigned(AfterLoaded) then
-      begin
-        TThread.ForceQueue(nil,
-          procedure
-          begin
-            AfterLoaded(Self);
-          end);
-      end;
-    end);
-end;
-
 procedure TBitmapHelper.SaveToFile(const AFileName, Ext: string);
 var
   Stream: TFileStream;
@@ -414,49 +234,6 @@ begin
   end;
 end;
 
-class procedure TBitmapHelper.SetLoaded(Url: string);
-var
-  i: Integer;
-begin
-  for i := Low(PictureCache) to High(PictureCache) do
-    if PictureCache[i].Url = Url then
-      PictureCache[i].Loaded := True;
-end;
-
-{ TBitmapCacheItem }
-
-constructor TBitmapCacheItem.Create;
-begin
-  inherited;
-  Image := nil;
-  Loaded := False;
-  Url := '';
-end;
-
-destructor TBitmapCacheItem.Destroy;
-begin
-  {$IFNDEF AUTOREFCOUNT}
-  if Assigned(Image) then
-    Image.Free;
-  {$ENDIF}
-  inherited;
-end;
-
-procedure TBitmapCacheItem.SetImage(const Value: TBitmap);
-begin
-  FImage := Value;
-end;
-
-procedure TBitmapCacheItem.SetLoaded(const Value: Boolean);
-begin
-  FLoaded := Value;
-end;
-
-procedure TBitmapCacheItem.SetUrl(const Value: string);
-begin
-  FUrl := Value;
-end;
-
 { TObjectOwner }
 
 procedure TObjectOwner.Notification(AComponent: TComponent; Operation: TOperation);
@@ -476,18 +253,15 @@ end;
 
 initialization
   TBitmap.Pool := TThreadPool.Create;
-  TBitmap.DroppingCache := False;
-  TBitmap.CacheSize := 60;
-  TBitmap.LoadCounterLimit := 20;
-  TBitmap.GlobalUseCache := False;
   TBitmap.FCallbackList := TThreadList<TCallbackObject>.Create;
   TBitmap.FObjectOwner := TObjectOwner.Create(nil);
+  TBitmap.FClient := nil;
 
 finalization
-  TBitmap.DropCache;
   TBitmap.Pool.Free;
   TBitmap.FCallbackList.Free;
   TBitmap.FObjectOwner.Free;
+  TBitmap.FClient.Free;
 
 end.
 
